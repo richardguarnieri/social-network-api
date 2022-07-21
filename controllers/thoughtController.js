b44da8a65fa0d01513c2a6787846274515cb8285
Richard Guarnieri <richard.gm@outlook.com>
2022-07-21 18:28:21 -0500

    modified postThought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,4 @@
-const { Thought } = require('./../models');
+const { Thought, User } = require('./../models');
 
 // GET all thoughts
 const getThoughts = async (req, res) => {
@@ -25,22 +25,18 @@ const getThought = async (req, res) => {
     }
 };
 
-// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+// POST to create a new thought (pushes the created thought's _id to the associated user's thoughts array field)
 const postThought = async (req, res) => {
     try {
-        const { thoughtText, username } = req.body;
-        const thought = await Thought.create({
-            thoughtText,
-            // createdAt,
-            username,
-            // reactions: {
-            //     reactionId,
-            //     reactionBody,
-            //     username,
-            //     createdAt
-            // }
-        })
-        res.status(200).json({success: true});
+        const { thoughtText, userId } = req.body;
+        if (userId.length !== 24) {return res.status(400).json({success: false, message: `Provided userId ${userId} is not a valid ID!`})};
+        const user = await User.findOne({_id: userId});
+        if (!user) {return res.status(400).json({success: false, message: `User with ID ${userId} does not exist!`})};
+        const thought = await Thought.create({thoughtText, username: user.username});
+        // push thought _id to user.thoughts array
+        user.thoughts.push(thought._id);
+        user.save();
+        res.status(200).json({success: true, message: `Thought ID ${thought._id} has been created and added to User ID ${userId}!`});
     } catch (err) {
         res.status(500).json({success: false, message: 'something went wrong...', error: err.message})
     }
