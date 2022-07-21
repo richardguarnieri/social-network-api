14f74742df31926d0e820a4f5aed8a0ac2e945a9
Richard Guarnieri <richard.gm@outlook.com>
2022-07-21 17:35:52 -0500

    added postUserFriend route

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,12 +13,12 @@ const getUsers = async (req, res) => {
 // GET a single user by its _id and populated thought and friend data
 const getUser = async (req, res) => {
     try {
-        const { id } = req.params;
+        const { userId } = req.params;
         // validates if id parameter is a valid MongoDB ObjectId
-        if (id.length !== 24) {return res.status(400).json({success: false, message: `Provided ID ${id} is not a valid ID!`})}
-        const user = await User.findOne({_id: id}).populate('friends');
+        if (userId.length !== 24) {return res.status(400).json({success: false, message: `Provided ID ${userId} is not a valid ID!`})}
+        const user = await User.findOne({_id: userId}).populate('friends');
         // validates if user is empty (null)
-        if (!user) {return res.status(400).json({success: false, message: `User with ID ${id} does not exist!`})}
+        if (!user) {return res.status(400).json({success: false, message: `User with ID ${userId} does not exist!`})}
         res.status(200).json(user);
     } catch (err) {
         res.status(400).json({success: false, message: 'Something went wrong...', error: err.message})
@@ -39,20 +39,20 @@ const postUser = async (req, res) => {
 // PUT to update a user by its _id
 const updateUser = async (req, res) => {
     try {
-        const { id } = req.params;
-        if (id.length !== 24) {return res.status(400).json({success: false, message: `Provided ID ${id} is not a valid ID!`})}
+        const { userId } = req.params;
+        if (userId.length !== 24) {return res.status(400).json({success: false, message: `Provided ID ${userId} is not a valid ID!`})}
         const { username, email } = req.body;
         // const user = await User.findOneAndUpdate(
         //     {_id: id},
         //     {$set: {username, email}},
         //     {runValidators: true, new: true}
         // );
-        const user = await User.findOne({_id: id});
-        if (!user) {return res.status(400).json({success: false, message: `User with ID ${id} does not exist!`})}
+        const user = await User.findOne({_id: userId});
+        if (!user) {return res.status(400).json({success: false, message: `User with ID ${userId} does not exist!`})}
         if (username) {user.username = username};
         if (email) {user.email = email};
         user.save()
-        res.status(200).json({success: true, message: `User with ID ${user.id} has been successfully updated!`})
+        res.status(200).json({success: true, message: `User with ID ${userId} has been successfully updated!`})
     } catch (err) {
         res.status(400).json({success: false, message: 'Something went wrong...', error: err.message})
     }
@@ -61,17 +61,42 @@ const updateUser = async (req, res) => {
 // DELETE to remove user by its _id
 const deleteUser = async (req, res) => {
     try {
-        const { id } = req.params;
-        if (id.length !== 24) {return res.status(400).json({success: false, message: `Provided ID ${id} is not a valid ID!`})}
-        const user = await User.findOne({_id: id});
-        if (!user) {return res.status(400).json({success: false, message: `User with ID ${id} does not exist!`})}
-        await User.deleteOne({_id: id});
-        res.status(200).json({success: true, message: `User with ID ${id} has been successfully deleted!`});
+        const { userId } = req.params;
+        if (userId.length !== 24) {return res.status(400).json({success: false, message: `Provided ID ${userId} is not a valid ID!`})}
+        const user = await User.findOne({_id: userId});
+        if (!user) {return res.status(400).json({success: false, message: `User with ID ${userId} does not exist!`})}
+        await User.deleteOne({_id: userId});
+        res.status(200).json({success: true, message: `User with ID ${userId} has been successfully deleted!`});
     } catch (err) {
         res.status(400).json({success: false, message: 'Something went wrong...', error: err.message})
     }
 };
 
+// POST to add a new friend to a user's friend list
+const postUserFriend = async (req, res) => {
+    try {
+        const { userId, friendId } = req.params;
+        if (userId.length !== 24 || friendId.length !== 24) {return res.status(400).json({success: false, message: `Provided IDs are not a valid IDs!`})}
+        const user = await User.findOne({_id: userId});
+        if (!user) {return res.status(400).json({success: false, message: `User with ID ${userId} does not exist!`})}
+        const friend = await User.findOne({_id: friendId});
+        if (!friend) {return res.status(400).json({success: false, message: `User with ID ${friendId} does not exist!`})}
+        // check if friendId is already included in the user's friend list
+        if (user.friends.includes(friendId)) {return res.status(400).json({success: false, message: `User ID ${friendId} is already friends with User ID ${userId}!`})}
+        user.friends.push(friendId);
+        user.save();
+        res.status(200).json({success: true, message: `User ID ${friendId} has been added to User ID ${userId} friends!`})
+    } catch (err) {
+        res.status(400).json({success: false, message: 'Something went wrong...', error: err.message})
+    }
+}
+
+// DELETE to remove a friend from a user's friend list
+const deleteUserFriend = async (req, res) => {
+    
+}
+
+
 module.exports = {
-    getUsers, getUser, postUser, updateUser, deleteUser
+    getUsers, getUser, postUser, updateUser, deleteUser, postUserFriend, deleteUserFriend
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, getUser, postUser, updateUser, deleteUser } = require('./../../controllers/userController');
+const { getUsers, getUser, postUser, updateUser, deleteUser, postUserFriend, deleteUserFriend } = require('./../../controllers/userController');
 
 // /api/users
 router.route('/')
     .get(getUsers)
     .post(postUser);
 
-// /api/users/:id
-router.route('/:id')
+// /api/users/:userId
+router.route('/:userId')
     .get(getUser)
     .put(updateUser)
     .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+    .post(postUserFriend)
+    .delete(deleteUserFriend);
 
 module.exports = router;
