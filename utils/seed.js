baecca95c6db4922a971fe6b51cb023282b9187b
Richard Guarnieri <richard.gm@outlook.com>
2022-07-22 10:59:41 -0500

    added console logs to seed.js execution

--- utils/seed.js
+++ utils/seed.js
@@ -6,9 +6,13 @@ const { User, Thought } = require('./../models');
 const seedData = async () => {
     await connection();
     await User.deleteMany();
+    console.log('\n----- USER COLLECTION HAS BEEN WIPED! -----');
     await Thought.deleteMany();
+    console.log('\n----- THOUGHT COLLECTION HAS BEEN WIPED! -----');
     await seedUsers();
+    console.log('\n----- USERS SEEDED SUCCESSFULLY! -----');
     await seedThoughts();
+    console.log('\n----- THOUGHTS SEEDED SUCCESSFULLY! -----\n');
     process.exit();
 }
 
