9921a3b8801ef045dcf9fb589e437ed7e540d5d7
Richard Guarnieri <richard.gm@outlook.com>
2022-07-22 01:53:22 -0500

    added collection drops to seed.js

--- utils/seed.js
+++ utils/seed.js
@@ -1,9 +1,12 @@
 const connection = require('./../config/connection');
 const seedUsers = require('./userSeed');
 const seedThoughts = require('./thoughtSeed');
+const { User, Thought } = require('./../models');
 
 const seedData = async () => {
     await connection();
+    await User.deleteMany();
+    await Thought.deleteMany();
     await seedUsers();
     await seedThoughts();
     process.exit();
